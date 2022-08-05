f6ac0d9b1597601b171f973df4e5c4fab8037cc3
Madhavi Thakur <madhavidadhwal91@gmail.com>
2022-08-05 17:21:53 -0400

    Single page applications part1

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -8,7 +8,7 @@ class Header extends Component {
         this.state = {
             isNavOpen: false
         }
-        this.toggleNav = this.toggleNav.bind(this);
+    //    this.toggleNav = this.toggleNav.bind(this);
     }
 
     toggleNav = () => {
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,9 +1,13 @@
 import Home from './HomeComponent';
 import Menu from './MenuComponent';
 import Dishdetail from "./DishdetailComponent";
+import Contact from './ContactComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import { DISHES } from '../shared/dishes';
+import { PROMOTIONS } from '../shared/promotions';
+import { LEADERS } from '../shared/leaders';
+import { COMMENTS } from '../shared/comments';
 import React, { Component } from 'react';
 import {Routes, Route} from 'react-router-dom';
 
@@ -11,14 +15,19 @@ class Main extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      dishes : DISHES
+      dishes : DISHES,
+      comments: COMMENTS,
+      promotions: PROMOTIONS,
+      leaders: LEADERS
     };
   }
   
   render(){
     const HomePage = () => {
         return (
-            <Home />
+            <Home dish={this.state.dishes.filter(dish=>dish.featured)[0]} 
+                  promotion={this.state.promotions.filter(promotion=>promotion.featured)[0]}
+                  leader={this.state.leaders.filter(leader=>leader.featured)[0]} />
         );
     }
     return (
@@ -27,6 +36,7 @@ class Main extends Component {
         <Routes>
             <Route path="/home" element={<HomePage/>} />
             <Route exact path="/menu" element={<Menu dishes={this.state.dishes} />} />
+            <Route exact path="/contactus" element={<Contact/>} />
             <Route path="*" element={<HomePage/>} />
         </Routes>
         <Footer/>
